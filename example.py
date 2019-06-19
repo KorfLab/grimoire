330afef9207a354836457ac92c735924a2023fb6
hslyman <hslyman@ucdavis.edu>
2019-06-19 12:45:28 -0700

    decoder added

--- example.py
+++ example.py
@@ -9,22 +9,33 @@ import toolbox.dna
 ### FASTA ###
 
 from toolbox.fasta import Fasta
-ff = Fasta('data/TAIR10_1.fasta')
-for id in ff.ids:
-	entry = ff.get(id)
-	print(entry.id, entry.desc, entry.seq[0:50])
+# ff = Fasta('data/TAIR10_1.fasta')
+# for id in ff.ids:
+# 	entry = ff.get(id)
+# 	print(entry.id, entry.desc, entry.seq[0:50])
 
 ### GFF ###
 
-from toolbox.gff import Gff
-gf = Gff('data/TAIR10_1.gff3')
-print(gf.types)
-print(gf.chroms)
-for f in gf.get(chrom='Chr1', beg=7500, end=8500):
-	print(f.beg, f.end, f.type, f.strand)
+# from toolbox.gff import Gff
+# gf = Gff('data/TAIR10_1.gff3')
+# print(gf.types)
+# print(gf.chroms)
+# for f in gf.get(chrom='Chr1', beg=7500, end=8500):
+# 	print(f.beg, f.end, f.type, f.strand)
 
 ## DNA ##
 
-seq = 'ACCCCGAGGAGAGGACCCATAGGC'
-rev = toolbox.dna.revcomp(seq)
-print(seq, rev)
+# seq = 'ACCCCGAGGAGAGGACCCATAGGC'
+# rev = toolbox.dna.revcomp(seq)
+# print(seq, rev)
+
+## HMM ##
+from hmm import HMM
+import viterbi
+hmm = HMM.read('toy.hmm')
+ff = Fasta('toy.fasta')
+for id in ff.ids:
+	seq = ff.get(id).seq
+	print(seq)
+	path, score = viterbi.decode(model=hmm, seq=seq, inspect=False)
+	print(path, score)
---
+++ hmm/viterbi.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+import sys
+
+import toolbox.fasta
+import toolbox.gff
+
+
+def inspector(model, matrix, beg, end):
+	for state in model.states:
+		print('{:<6s}'.format(state.name), end='')
+		for i in range(beg, end):
+			print('{:<10.3g}'.format(matrix[i][state.name]['score']), end='')
+		print()
+			
+def decode(model=None, seq=None, inspect=False):
+	# transition matrix
+	tm = {}
+	for state in model.states:
+		tm[state.name] = {}
+	for s1 in model.states:
+		for s2 in s1.next:
+			tm[s2][s1.name] = s1.next[s2]
+	# set up data structure
+	viterbi = []
+	for i in range(len(seq) + 1):
+		viterbi.append({})
+		for state in model.states:
+			viterbi[i][state.name] = {'score' : None, 'trace' : None}
+	# initialize matrix
+	for state in model.states:
+		viterbi[0][state.name]['score'] = state.init
+
+	# fill in matrix
+	for pos in range(1, len(seq) + 1):
+		nt = seq[pos - 1 : pos]
+		#print(pos)
+		for this in model.states:
+			max_score = 0
+			max_state = None
+			for prev in model.states:
+				if this.name in prev.next:
+					tp = tm[this.name][prev.name]
+					pp = viterbi[pos - 1][prev.name]['score']
+					ep = None
+					if this.ctxt == 0:
+						ep = this.emit[nt] if nt in this.emit else 0.25
+					else:
+						if pos - 1 > this.ctxt:
+							ctx = seq[pos - this.ctxt - 1: pos - 1]
+							ep = this.emit[ctx][nt] if ctx in this.emit and nt in this.emit[ctx] else 0.25
+						else:
+							ep = 0.25
+					score = tp * pp * ep
+				if score > max_score:
+					max_score = score
+					max_state = prev.name
+					#print(prev.name, this.name, max_score)
+	
+			viterbi[pos][this.name]['score'] = max_score
+			viterbi[pos][this.name]['trace'] = max_state
+	# terminate
+	max_score = 0
+	max_state = None
+	for state in model.states:
+		viterbi[-1][state.name]['score'] *= state.term
+		if viterbi[-1][state.name]['score'] > max_score:
+			max_score = viterbi[-1][state.name]['score']
+			max_state = state.name
+	if inspect:
+		inspector(model, viterbi, 0, len(seq) + 1)	
+	# traceback
+	pos = len(seq)
+	path = []
+	prev = viterbi[pos][max_state]['trace']
+	# viterbi position 0 is the initial column. do not append to path
+	while pos > 0:
+		path.append(prev)
+		prev = viterbi[pos][max_state]['trace']
+		max_state = prev
+		pos -= 1
+	path.reverse()
+	return(path, max_score)
+	
+	
