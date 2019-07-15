54fcffe0e465c15392c91a15429e40b428f8d2ef
Alex Kramer <akramer@ucdavis.edu>
2019-07-14 23:26:18 -0700

    Add test for f-b algorithm (prob. space)

--- grimoire/test/test_hmm_decode.py
+++ grimoire/test/test_hmm_decode.py
@@ -9,10 +9,10 @@ import grimoire.hmm as hmm
 import grimoire.decode as decode
 
 class TestHMM(unittest.TestCase):
-	
+
 	def setUp(self):
 		self.hmm = None
-	
+
 		gen = genome.Genome(
 			gff3='data/C.elegans.1percent.gff3',
 			fasta='data/C.elegans.1percent.fasta')
@@ -29,7 +29,7 @@ class TestHMM(unittest.TestCase):
 		donor_emits = hmm.train_emissions(donor_seqs, context=0)
 		donor_states = hmm.state_factory('DON', donor_emits)
 		hmm.connect_all(donor_states)
-		
+
 		genome_emits = hmm.train_emission(null_seqs, context=1)
 		genome_state = hmm.State(name='GEN', context=1, emits=genome_emits)
 		genome_state.init = 1
@@ -37,13 +37,13 @@ class TestHMM(unittest.TestCase):
 		hmm.connect2(genome_state, genome_state, 0.99)
 		hmm.connect2(genome_state, donor_states[0], 0.01)
 		hmm.connect2(donor_states[-1], genome_state, 1)
-		
-		null_emits = hmm.train_emission(chr.seq, context=0)	
+
+		null_emits = hmm.train_emission(chr.seq, context=0)
 		null_state = hmm.State(name='NULL', context=0, emits=null_emits)
 		null_state.init = 1
 		null_state.term = 1
 		hmm.connect2(null_state, null_state, 1)
-				
+
 		self.hmm = hmm.HMM(name='test', null=null_state,
 			states=donor_states + [genome_state])
 
@@ -68,7 +68,7 @@ class TestHMM(unittest.TestCase):
 		ft = p.features(labels=['DON', 'GEN'], dna=dna)
 		self.assertEqual(ft[1].type, 'DON')
 		self.assertEqual(ft[1].beg, 5)
-	
+
 	def test_Decode_Viterbi_logspace(self):
 		dna = sequence.DNA(name='test', seq='AAAAGTAAGTTTTT')
 		m = copy.deepcopy(self.hmm)
@@ -83,7 +83,7 @@ class TestHMM(unittest.TestCase):
 		ps = v.generate_paths(1000)
 		self.assertEqual(ps[0].score, 0.8980462178059174)
 		self.assertEqual(ps[0].freq, 0.524)
-	
+
 	def test_Decode_StochasticViterbi_logspace(self):
 		dna = sequence.DNA(name='test', seq='TTTTTGTAAGTAAGTTTTT')
 		m = copy.deepcopy(self.hmm)
@@ -93,7 +93,17 @@ class TestHMM(unittest.TestCase):
 		self.assertEqual(ps[0].score, -0.10753374451446263)
 		self.assertEqual(ps[0].freq, 0.524)
 
+	def test_Decode_ForwardBackward(self):
+		dna = sequence.DNA(name='test', seq='AGT')
+		p = decode.ForwardBackward(model=self.hmm, dna=dna)
+		self.assertAlmostEqual(p.posterior(time=0, state='GEN'),
+			0.01224654564, places=8)
+		self.assertAlmostEqual(p.posterior(time=2, state='DON-1'),
+			0.0025, places=8)
+		self.assertAlmostEqual(p.posterior(time=1, state='DON-0'),
+			0.0025, places=8)
+		self.assertAlmostEqual(p.posterior(time=2, state='GEN'),
+			0.009746545644, places=8)
+
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
-
-
