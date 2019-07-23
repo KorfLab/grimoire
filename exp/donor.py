d704fe668b2f1882ba2540eec8678d0322e6d273
Ian Korf <ifkorf@ucdavis.edu>
2019-07-23 00:43:59 -0700

    3.5 compat and more actual experiment

--- exp/donor.py
+++ exp/donor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import copy
-from subprocess import run
+import subprocess
 import re
 
 CMD = [
@@ -12,8 +12,8 @@ CMD = [
 	'--replicant']
 
 print('\t'.join(['dlen', 'dctx', 'ictx', 'ectx', 'perf']))
-for dl in range(8, 9):
-	for dc in range(0, 1):
+for dl in range(2, 9):
+	for dc in range(0, 2):
 		for ic in range(0, 2):
 			for ec in range(0, 2):
 				cmd = copy.copy(CMD)						
@@ -25,7 +25,7 @@ for dl in range(8, 9):
 				cmd.append(str(ic))
 				cmd.append('--exon_ctx')
 				cmd.append(str(ec))
-				p = run(cmd, capture_output=True)
+				p = subprocess.run(cmd, stdout=subprocess.PIPE)
 				out = str(p.stdout, 'utf-8')
 				m = re.search('Accuracy: (\S+)', out)
 				print('\t'.join([str(dl), str(dc), str(ic), str(ec), m[1]]))
