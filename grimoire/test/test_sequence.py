f74bf3ffb425211d052ef475a19ec1daba5edea7
hslyman <hslyman@ucdavis.edu>
2019-08-07 13:28:16 -0700

    added assertions so exceptions raised causes test FAIL (rather than than ERROR)

--- grimoire/test/test_sequence.py
+++ grimoire/test/test_sequence.py
@@ -7,15 +7,27 @@ class TestSequence(unittest.TestCase):
 	
 	def test_DNA(self):
 		s = sequence.DNA(seq='CATGCCAACAATGCNCAY')
-		s.check_alphabet()
+		self.assertIsInstance(s, sequence.DNA)
+		try:
+			s.check_alphabet()
+		except sequence.SequenceError:
+			self.fail('DNA object failed check_alphabet()')
 		p = s.translate()
-		p.check_alphabet()
+		self.assertIsInstance(p, sequence.Protein)
 		self.assertEqual(p.seq, 'HANNAH')
+		s.revcomp()
+		self.assertIsInstance(s, sequence.DNA)
+		self.assertEqual(s.seq, 'RTGNGCATTGTTGGCATG')
+
 		
 	def test_Protein(self):
 		s = sequence.Protein(seq='MDDDIAALVVDNGSGMCKAGFAGDDAP',
 			name='NP_990849.1')
-		s.check_alphabet()
+		self.assertIsInstance(s, sequence.Protein)
+		try:
+			s.check_alphabet()
+		except sequence.SequenceError:
+			self.fail('Protein object failed check_alphabet()')
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
