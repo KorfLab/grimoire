d2a31862bff021a9a38860ff41036ad5c8a03d81
hslyman <hslyman@ucdavis.edu>
2019-08-07 13:36:14 -0700

    added some assertions

--- grimoire/test/test_io.py
+++ grimoire/test/test_io.py
@@ -7,6 +7,7 @@ class TestIO(unittest.TestCase):
 
 	def test_FASTA_entry(self):
 		entry = io.FASTA_entry('foo', 'bar', 'ACGT')
+		self.assertIsInstance(entry, io.FASTA_entry)
 		s = entry.string(wrap=2)
 		self.assertEqual(s, '>foo bar\nAC\nGT\n')
 	
@@ -14,6 +15,7 @@ class TestIO(unittest.TestCase):
 		if os.system('gunzip -c data/C.elegans.1percent.fasta > data/test.fasta'):
 			raise 'unable to create test file'
 		ff = io.FASTA_file('data/test.fasta')
+		self.assertIsInstance(ff, io.FASTA_file)
 		c4 = ff.get('IV')
 		self.assertEqual(len(c4.seq), 174938)
 		os.system('rm data/test.fasta')
@@ -31,6 +33,7 @@ class TestIO(unittest.TestCase):
 		
 	def test_GFF_entry(self):
 		e = io.GFF_entry('I	WormBase	gene	3747	3909	.	-	.	ID=Gene:WBGene00023193;Name=WBGene00023193;interpolated_map_position=-21.9001;sequence_name=Y74C9A.6;biotype=snoRNA;so_term_name=snoRNA_gene;curie=WB:WBGene00023193;Alias=Y74C9A.6')
+		self.assertIsInstance(e, io.GFF_entry)
 		self.assertEqual(e.source, 'WormBase')
 		with self.assertRaises(io.GFF_skip):
 			c = io.GFF_entry('#comment line')
@@ -41,11 +44,13 @@ class TestIO(unittest.TestCase):
 		
 	def test_GFF_file(self):
 		gff = io.GFF_file('data/A.thaliana.1percent.gff3.gz')
+		self.assertIsInstance(gff, io.GFF_file)
 		stuff = gff.get(chrom='Chr1', type='exon', beg=5000, end=9999)
 		self.assertEqual(len(stuff), 20)
 
 	def test_GFF_stream(self):
 		gff = io.GFF_stream('data/C.elegans.1percent.gff3.gz')
+		self.assertIsInstance(gff, io.GFF_stream)
 		count = 0
 		for e in gff:
 			if e.type == 'exon': count += 1
