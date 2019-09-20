e71e93756af4e1a1f771021297c4637f70aaac48
hslyman <hslyman@ucdavis.edu>
2019-09-20 10:36:37 -0700

    added some tests that are probably redundant

--- grimoire/test/test_hmm.py
+++ grimoire/test/test_hmm.py
@@ -2,6 +2,7 @@
 import unittest
 import copy
 import os
+import json
 
 import grimoire.sequence as sequence
 import grimoire.genome as genome
@@ -15,6 +16,7 @@ class TestHMM(unittest.TestCase):
 		
 		donor_seqs = []
 		gen_seqs = []
+		cds_seqs = []
 
 		gen = genome.Reader(
 			gff='data/ce270.gff3.gz',
@@ -26,6 +28,7 @@ class TestHMM(unittest.TestCase):
 				if gene.issues: continue
 				for mrna in gene.transcripts():
 					if not mrna.is_coding: continue
+					cds_seqs.append(mrna.cds_str())
 					for intron in mrna.introns:
 						donor_seqs.append(intron.seq_str()[0:6])
 		
@@ -45,7 +48,48 @@ class TestHMM(unittest.TestCase):
 
 		self.hmm = hmm.HMM(name='test', null=null_state,
 			states=donor_states + [genome_state])
-
+		self.seqs = donor_seqs
+		self.cdss = cds_seqs
+		
+	def test_emission_model(self):
+		em = hmm.emission_model(context=2, alphabet='nt')
+		self.assertEqual(len(em), 16)
+		self.assertEqual(em['AA']['A'], 0)
+		with self.assertRaises(hmm.HMMError):
+			em = hmm.emission_model(context=-1, alphabet='nt')
+		
+	def test_train_emission(self):
+		em = hmm.train_emission(self.seqs, context=1)
+		self.assertEqual(len(em), 4)
+		self.assertEqual(em['A']['A'], 0.2994)
+		with self.assertRaises(hmm.HMMError):
+			em = hmm.train_emission(self.seqs, context=-1)
+		
+	def test_train_emissions(self):
+		em = hmm.train_emissions(self.seqs, context=1)
+		self.assertEqual(len(em), len(self.seqs[0]))
+		self.assertEqual(len(em[0]), 4)
+		with self.assertRaises(hmm.HMMError):
+			em = hmm.train_emission(self.seqs, context=-1)
+		
+	def test_train_cds(self):
+		cds = hmm.train_cds(self.cdss, context=2)
+		# there should be very few stop codons in frame
+		self.assertTrue(cds[2]['TA']['A'] < cds[2]['TA']['T'])	
+		
+	def test_state_factory_and_connect_all(self):
+		sl = hmm.state_factory('DON', hmm.train_emissions(self.seqs, context=0))
+		self.assertEqual(len(sl), 6)
+		self.assertEqual(sl[0].name, 'DON-0')
+		hmm.connect_all(sl)
+		self.assertEqual(sl[0].next['DON-1'], 1)
+		
+	def test_connect2(self):
+		s1 = hmm.State(name='s1')
+		s2 = hmm.State(name='s2')
+		hmm.connect2(s1, s2, 0.5)
+		self.assertEqual(s1.next['s2'], 0.5)
+		
 	def test_HMM_emit(self):
 		self.assertEqual(self.hmm.null.emit['A'], 0.3197)
 	
@@ -67,11 +111,13 @@ class TestHMM(unittest.TestCase):
 		with self.assertRaises(hmm.HMMError):
 			m1.convert2log()
 
-
-	def test_Viterbi(self):
+	def test_Viterbi_and_Parse(self):
 		dna = sequence.DNA(name='test', seq='AAAAGTAAGTTTTT')
 		v = hmm.Viterbi(model=self.hmm, dna=dna)
 		p = v.generate_path()
+		self.assertIsInstance(p, hmm.Parse)
+		self.assertEqual(p.freq, None)
+		self.assertEqual(p.path[0], 'GEN')
 		self.assertAlmostEqual(p.score, 4.837415437963225)
 		ft = p.features()
 		self.assertEqual(ft[1].type, 'DON')
@@ -85,10 +131,11 @@ class TestHMM(unittest.TestCase):
 		p = v.generate_path()
 		self.assertAlmostEqual(p.score, 1.5763805776787159)
 
-	def test_StochasticViterbi(self):
+	def test_StochasticViterbi_and_Parse(self):
 		dna = sequence.DNA(name='test', seq='TTTTTGTAAGTAAGTTTTT')
 		v = hmm.StochasticViterbi(model=self.hmm, dna=dna, seed=1)
 		ps = v.generate_paths(1000)
+		self.assertIsInstance(ps[0], hmm.Parse)
 		self.assertAlmostEqual(ps[0].score, 4.600333948735676)
 		self.assertAlmostEqual(ps[0].freq, 0.459)
 
@@ -119,11 +166,8 @@ class TestHMM(unittest.TestCase):
 		dna.ftable.add_feature(Feature(dna, 4, 9, '+', 'DON'))
 		dna.ftable.add_feature(Feature(dna, 10, 12, '+', 'GEN'))
 		tc = hmm.Transcoder(model=self.hmm, dna=dna)
-		self.assertEqual(tc.score, 1.250948871669515)
+		self.assertAlmostEqual(tc.score, 1.250948871669515)
 		
-	def test_Parse(self):
-		pass
-		# needs testing, by maybe reorganizing instead
 	
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
