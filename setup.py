672d2cb9d56a1cc32a5c81c7c0cee2d535c2ace7
Ian Korf <ifkorf@ucdavis.edu>
2019-09-25 10:51:29 -0700

    0.3 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
 	name='grimoire',
-	version='0.0.1',
+	version='0.0.3',
 	author='Ian Korf',
 	author_email='ifkorf@gmail.com',
 	description='Biological sequence analysis tools',
@@ -18,5 +18,6 @@ setuptools.setup(
 		'License :: OSI Approved :: MIT License',
 		'Operating System :: OS Independent',
 	],
+	python_requires='>=3.6',
 )
 
